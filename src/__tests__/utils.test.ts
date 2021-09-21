f3c50aebeb27d326168a221f75bc4bcf72bf7fc1
dani <dan.yaniv@bitmovin.com>
2021-09-21 00:34:59 -0400

    cleaned

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -7,13 +7,4 @@ describe('utils', () => {
       expect(message).toEqual('message 1');
     });
   });
-  /* at the end, I use Event interface and not a class
-  const event: Event = {"type": "EVENT_TYPE_1", message: "abc"};
-  describe('utils#Event#accessors', () => {
-    it('get and set message', () => {
-      const message = "value";
-      event.message = message; 
-      expect(event.message).toEqual(message);
-    });
-  });*/
 });
